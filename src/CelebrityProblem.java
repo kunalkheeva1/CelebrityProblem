7f183ddda7fa789811ed2dd9052f944997b6f5d4
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 20:04:33 -0700

    added comments

--- src/CelebrityProblem.java
+++ src/CelebrityProblem.java
@@ -1,8 +1,12 @@
 public class CelebrityProblem {
 
+    //celeb problem
     public static int celebrity(int [][] arr, int n){
+
+        // get two pointers to revolve
         int i= 0;
         int j=n-1;
+        // if one knows other then move forward
         while(i<j){
             if(arr[i][j] == 1){
                 i++;
@@ -10,9 +14,14 @@ public class CelebrityProblem {
                 j--;
             }
         }
+
+        // when i find celeb
         int celeb =i;
         for(i=0; i<n; i++){
+            // celeb cannot know itself
             if(i!= celeb){
+
+                // if someone does not know celeb and the celeb knows someone then its not celeb
                 if(arr[i][celeb]==0 || arr[celeb][i] ==1){
                     return -1;
                 }
